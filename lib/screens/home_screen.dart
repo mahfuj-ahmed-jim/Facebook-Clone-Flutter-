05e7e9179a91868e7478232d39cc12e4ecbefe47
mahfuj-ahmed-jim <mahfujahmedjim32@gmail.com>
2022-01-30 15:10:47 +0600

    Sliver AppBar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,48 @@
+import 'package:facebook_clone/config/palette.dart';
+import 'package:facebook_clone/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            brightness: Brightness.light,
+            backgroundColor: Colors.white,
+            title: Text(
+              'facebook',
+              style: TextStyle(
+                fontSize: 28,
+                color: Palette.facebookBlue,
+                fontWeight: FontWeight.bold,
+                letterSpacing: -1.2,
+              ),
+            ),
+            centerTitle: false,
+            floating: true,
+            actions: [
+              CircleButton(
+                  icon: Icons.search,
+                  iconSize: 30,
+                  onPressed: () {
+                    print('Search');
+                  }),
+              CircleButton(
+                  icon: MdiIcons.facebookMessenger,
+                  iconSize: 30,
+                  onPressed: () {
+                    print('Messanger');
+                  })
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
